3cd11da3bf49bb312b7967a97147249dbaf8f870
Kyllian Mobley <kyllian@users.noreply.github.com>
2025-10-20 20:29:25 -0500

    Style updates

--- app/app/search/components/SearchInput.tsx
+++ app/app/search/components/SearchInput.tsx
@@ -8,14 +8,18 @@ export interface SearchInputProps {
 
 const SearchInput: React.FC<SearchInputProps> = ({q}) => (
     <div className="navbar p-0">
-        <Link href="/" className="btn btn-link btn-sm px-0 mr-2 text-base-content"><Image src="/icon.svg" alt="Edict" width={35} height={35}/></Link>
+        <Link href="/" className="btn btn-link btn-sm px-0 mr-2"><Image src="/icon.svg" alt="Edict" width={35} height={35}/></Link>
         <fieldset className="fieldset rounded-box w-full flex-1">
             <label className="input input-md w-full">
                 <input type="search"
                        name="q"
-                       placeholder="Search"
                        defaultValue={q}
                        className="ml-2"/>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                     stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                          d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"/>
+                </svg>
             </label>
         </fieldset>
     </div>
