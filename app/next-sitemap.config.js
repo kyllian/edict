4ea1d6a3cfba5eebe554c860a01437f82c9dab98
Kyllian Mobley <kyllian@users.noreply.github.com>
2025-10-23 22:14:29 -0500

    Remove extra package and config

--- app/next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://edict.kyllian.xyz',
-    generateRobotsTxt: true,
-    changefreq: 'weekly',
-    priority: 0.7,
-    sitemapSize: 5000,
-};
